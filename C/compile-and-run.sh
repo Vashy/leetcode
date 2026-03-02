2feb14d9369d8e0a13e209f26eef801eac2d0b83
Timoty Granziero <timoty.granziero@protonmail.com>
2026-03-02 21:09:37 +0100

    chore: remove cmake

--- C/compile-and-run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-. ./compile.sh
-. ./run.sh
--- C/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-BUILD_DIR=./build
-
-set -x
-
-cmake -DCMAKE_BUILD_TYPE=Debug -S . -G "Unix Makefiles" -B $BUILD_DIR
-
-make --directory $BUILD_DIR
